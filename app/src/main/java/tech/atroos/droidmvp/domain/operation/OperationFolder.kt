d563284625ac79a0f47b683e6f36d533c992c899
SabNK <sabnk@optictelecom.ru>
2023-05-29 18:14:33 +0300

    OperationFolder

---
+++ app/src/main/java/tech/atroos/droidmvp/domain/operation/OperationFolder.kt
@@ -0,0 +1,9 @@
+package tech.atroos.droidmvp.domain.operation
+
+class OperationFolder (val name: String, val contents: List<OperationComponent>): OperationComponent {
+
+}
+
+interface OperationComponent {
+
+}
